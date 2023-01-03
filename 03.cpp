0b1dbc374fc152d26885e473e13b559ad4c2bcc4
Uzer Qureshi <uq0803@gmail.com>
2023-01-03 08:06:52 +0530

    sixth commit

---
+++ 03.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // This is my first addation code in c++  _________This comment is also called as Single line Comment
+    int a=24;
+    float b=12.67;
+    float c=a+b;
+    cout<<c;
+    return 0;
+}
---
+++ 04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+// Types of Variable present in the C++
+/*
+1- int= real numbers living decimals and fraction
+2-float=small decimal number
+3-double=large decimal number
+4-boolean=true or false
+5- char="p","q","r",etc
+*/ 
+    return 0;
+}
